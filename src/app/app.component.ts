c89382ff6bc4f6c1cc18d6fb62fea6fc0f876733
Rene Hinojosa <rphs09@hotmail.com>
2018-01-22 01:03:51 -0600

    Initialization of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.pug',
+  styleUrls: ['../sass/module/app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'TestTitle';
+
+  ngOnInit() {
+    console.log(_.chunk(['a', 'b', 'c', 'd'], 2));
+ }
+
 }
