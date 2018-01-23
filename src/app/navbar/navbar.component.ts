a8c91b20b297afdc4e499613f185246f3a36f1e0
Rene Hinojosa <rphs09@hotmail.com>
2018-01-23 00:53:17 -0600

    Added new navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.pug',
+  styleUrls: ['../../sass/module/navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
